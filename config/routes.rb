d66da4e6b802fe900fa803618677d99811853301
simon-group <saku_crownheart@yahoo.co.jp>
2019-07-11 16:35:46 +0900

    miniapp commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root  'tweets#index'
+  get   'tweets'      =>  'tweets#index'
+  get   'tweets/new'  =>  'tweets#new'
+  post  'tweets'      =>  'tweets#create'
+  delete  'tweets/:id' => 'tweets#destroy'
+  patch   'tweets/:id'  =>  'tweets#update'
+  get   'tweets/:id/edit' =>  'tweets#edit'
+  get   'users/:id'   =>  'users#show'    
 end
